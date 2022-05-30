aaf02e21681404cecbe5f049dd714e795920d14c
Simon <94635080+simn99@users.noreply.github.com>
2022-05-30 23:08:12 +0200

    Update singlelinkedlist.py

--- singlelinkedlist.py
+++ singlelinkedlist.py
@@ -2,14 +2,17 @@
 
 #Single linked list 
 
-class Node(): # parent class 
+from numpy import nonzero
+
+
+class Node(): # paretn class 
     
     def __init__(self,data):
 
         self.data = data
         self.next = None
 
-class linkedlist(Node): #children class
+class linkedlist(Node): #child class
     
     def __init__(self):
         self.head = None
@@ -30,7 +33,29 @@ class linkedlist(Node): #children class
             self.tail.next = newnode ##tail's next will point to the newly added node
             self.tail = newnode 
 
-    
+    def delete(self,data):
+        
+        current = self.head
+        if current is not None:
+            if current.data == data:
+                self.head =current.next
+                return
+        
+        # Search for the key to be deleted,
+        while (current is not None):
+
+            if current.data ==data:
+                break
+            prev = current
+            current = current.next
+
+        prev.next= current.next
+        
+        #if data was not present in linked list 
+
+        if current ==None: 
+            return
+
     def show(self): 
         #will display the nodes present in the list
         current =self.head
@@ -42,12 +67,18 @@ class linkedlist(Node): #children class
             current = current.next
 
 
+def main():
 
-obj = linkedlist()
-obj.add_node(2)
-obj.add_node(4)
+    obj = linkedlist()
+    obj.add_node(2)
+    obj.add_node(5)
 
-obj.show()
+    obj.add_node(20)
+    obj.delete(5)
+    obj.show()
+
+if __name__ == "__main__":
+    main()
 
 
 
